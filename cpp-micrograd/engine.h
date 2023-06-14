5454f253f0f99237dfcc028c8fdabb697c2d2359
10-zin <tenzinbhotia0@gmail.com>
2023-06-14 14:01:32 +0530

    disentangle cpp as cpp+header file

---
+++ cpp-micrograd/engine.h
@@ -0,0 +1,31 @@
+#ifndef SCRATCH_ENGINE_H
+#define SCRATCH_ENGINE_H
+
+#include <functional>
+#include <unordered_set>
+#include <string>
+#include <memory>
+
+class Value;
+
+std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, const std::shared_ptr<Value>& rhs);
+std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& lhs, const std::shared_ptr<Value>& rhs);
+
+class Value : public std::enable_shared_from_this<Value> {
+private:
+    double data;
+    double grad;
+    std::function<void()> backward;
+    std::unordered_set<std::shared_ptr<Value>> prev;
+    std::string op;
+
+public:
+    Value(double data, std::unordered_set<std::shared_ptr<Value>> prev = {}, std::string op = "");
+
+    double get_data() const;
+
+    std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& other);
+    std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& other);
+};
+
+#endif  // SCRATCH_ENGINE_H
