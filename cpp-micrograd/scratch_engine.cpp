875e81d9889ce13ab912b7fdd11678c1d52a4519
10-zin <tenzinbhotia0@gmail.com>
2023-06-14 13:25:01 +0530

    added inherent constructor

--- cpp-micrograd/scratch_engine.cpp
+++ cpp-micrograd/scratch_engine.cpp
@@ -13,24 +13,32 @@
 
 
 class Value : public std::enable_shared_from_this<Value> {
-public:
+private:
+    // Private member variables
     double data;
     double grad;
     std::function<void()> backward;
     std::unordered_set<std::shared_ptr<Value>> prev;
     std::string op;
 
-    static std::shared_ptr<Value> create(double data, std::unordered_set<std::shared_ptr<Value>> prev = {}, std::string op = "") {
-        return std::make_shared<Value>(Value(data, std::move(prev), std::move(op)));
-    }
-
-private:
-    Value(double data, std::unordered_set<std::shared_ptr<Value>> prev, std::string op)
-        : data(data), grad(0.0), prev(std::move(prev)), op(std::move(op)) {}
-
 public:
+    // Constructor
+    Value(double data, std::unordered_set<std::shared_ptr<Value>> prev = {}, std::string op = ""){
+        this->data = data;
+        this->grad = 0.0;
+        this->prev = std::move(prev);
+        this->op = std::move(op);
+    }
+    // Public getter for private data member variable
+    double get_data() const {
+        return data;
+    }
+    // Overriden logic of ADD and MUL operators
     std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& other) {
-        auto out = Value::create(data + other->data, std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other}, "+");
+        prev = std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other};
+
+        auto out = std::make_shared<Value>(data+other->data, prev, "+");
+
         out->backward = [this, other, out] {
             grad += out->grad;
             other->grad += out->grad;
@@ -38,15 +46,21 @@ public:
         return out;
     }
     std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& other){
-        auto out = Value::create(data*other->data, std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other}, "*");
+        prev = std::unordered_set<std::shared_ptr<Value>>{shared_from_this(), other};
+
+        auto out = std::make_shared<Value>(data*other->data, prev, "*");
+
         out->backward = [this, other, out] {
             grad += other->data*out->data;
             other->grad += data*out->data;
         };
         return out;
     }
-
 };
+
+//  Defining skeleton outside Value Class scope to support the syntax value1 + value2 and value1 * value2,
+//  where value1 and value2 are objects of the Value class.
+//  sort-of what happens: *lhs dereferences to std::shared_ptr<Value> and then calls Value's operator+ with rhs as argument.
 
 std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, const std::shared_ptr<Value>& rhs) {
     return (*lhs) + rhs;
@@ -58,8 +72,10 @@ std::shared_ptr<Value> operator*(const std::shared_ptr<Value>& lhs, const std::s
 
 int main() {
     // Create two Value objects
-    std::shared_ptr<Value> value1 = Value::create(2.5);
-    std::shared_ptr<Value> value2 = Value::create(3.7);
+    // Value value1(2.5);
+    // Value value2(3.7);
+    std::shared_ptr<Value> value1 = std::make_shared<Value>(2.5);
+    std::shared_ptr<Value> value2 = std::make_shared<Value>(3.7);
 
     // Perform addition using the operator+
     std::shared_ptr<Value> result_add = value1 + value2;
@@ -68,8 +84,8 @@ int main() {
     std::shared_ptr<Value> result_mul = value1 * value2;
 
     // Access the result and print the data
-    std::cout << "Result ADD: " << result_add->data << std::endl;
-    std::cout << "Result MUL: " << result_mul->data << std::endl;
+    std::cout << "Result ADD: " << result_add->get_data() << std::endl;
+    std::cout << "Result MUL: " << result_mul->get_data() << std::endl;
 
     return 0;
 }
