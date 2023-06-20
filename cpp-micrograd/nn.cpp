ea011f9b66cf198426b3f2397f255777a79044a3
10-zin <tenzinbhotia0@gmail.com>
2023-06-20 14:40:02 +0530

    first complete mlp version

--- cpp-micrograd/nn.cpp
+++ cpp-micrograd/nn.cpp
@@ -34,7 +34,7 @@ class Neuron: public Module{
                 this->weights.emplace_back(weight);
             }
         }
-        std::shared_ptr<Value> operator()(const std::vector<std::shared_ptr<Value>>& x){
+        std::shared_ptr<Value> operator()(std::vector<std::shared_ptr<Value>>& x){
             std::shared_ptr<Value> act = std::make_shared<Value>(0.0);;
             for (int i=0; i<x.size(); ++i){
                 act = act + (x[i]*weights[i]);
@@ -67,38 +67,106 @@ class Neuron: public Module{
     
 };
 
-// class Layer {
-//     private:
-//         std::vector<Neuron> neurons;
+class Layer: public Module{
+    private:
+        std::vector<Neuron> neurons;
+        int total_params;
 
-//     public:
-//         Layer(int nout, int nin){
-//             neurons.reserve(neurons.size()+1);
+    public:
+        Layer(int nin, int nout){
+            total_params=nin*nout;
+            neurons.reserve(neurons.size()+1);
 
-//             for (int i=0; i< nout; ++i){
-//                 Neuron neuron(nin, true);
-//                 neurons.emplace_back(neuron);
-//             }
-//         }
+            for (int i=0; i< nout; ++i){
+                Neuron neuron(nin, true);
+                neurons.emplace_back(neuron);
+            }
+        }
 
-// };
+        std::vector<std::shared_ptr<Value>> operator()(std::vector<std::shared_ptr<Value>> x){
+            std::vector<std::shared_ptr<Value>> out;
+            out.reserve(neurons.size()+1);
+            for (auto& neuron: neurons){
+                out.emplace_back(neuron(x));
+            }
+            return out;
+        }
+
+        std::vector<std::shared_ptr<Value>> parameters() override {
+            std::vector<std::shared_ptr<Value>> parameters;
+            parameters.reserve(total_params + 1);
+
+            for (auto neuron : neurons) {
+                for(auto param:  neuron.parameters()){
+                    parameters.emplace_back(param);
+                }
+            }
+
+            return parameters;
+        }
+
+};
+
+class MLP: public Module{
+    private:
+        std::vector<Layer> layers;
+        int total_params;
+    public:
+        MLP(int nin, std::vector<int> nout) {
+            layers.reserve(nout.size()+1);
+            total_params=1;
+
+            for (int i=0; i<nout.size(); ++i){
+                if (i==0){
+                    layers.emplace_back(Layer(nin, nout[i]));
+                    total_params=total_params*nin*nout[i];
+                }
+                else{
+                    layers.emplace_back(Layer(nout[i-1], nout[i]));
+                    total_params=total_params*nout[i-1]*nout[i];
+                }
+            }
+        }
+
+        std::vector<std::shared_ptr<Value>> operator()(std::vector<std::shared_ptr<Value>> x){
+            for (auto layer: layers){
+                x = layer(x);
+            }
+            return x;
+        }
+
+        std::vector<std::shared_ptr<Value>> parameters() override {
+            std::vector<std::shared_ptr<Value>> parameters;
+            parameters.reserve(total_params + 1);
+
+            for (auto layer : layers) {
+                for(auto param:  layer.parameters()){
+                    parameters.emplace_back(param);
+                }
+            }
+
+            return parameters;
+        }
+
+};
 
 int main(){
-    Neuron neuron(3, true);
-    neuron.show_parameters();
+    int nin=3;
+    std::vector<int> nout {6, 3, 1};
+    // Neuron neuron(3, true);
+    // neuron.show_parameters();
+    auto mlp = MLP(nin, nout);
     std::vector<std::shared_ptr<Value>> inputs;
     for (int i=0; i < 3; ++i){
         std::shared_ptr<Value> inp_val = std::make_shared<Value>(i);
         inputs.push_back(inp_val);
     } 
-    auto act = neuron(inputs);
-    std::cout<<act->get_data();
-    neuron.zero_grad();
+    auto output = mlp(inputs);
+    for(auto op: output){
+        std::cout<<"MLP O/P";
+        std::cout<<op->get_data();
+    }
     return 0;
 };
 
-// class MLP {
-//     private:
-//         int nin;
-//         std::vector<int> nouts;
-//         std::vector<Layer> layers;
+
