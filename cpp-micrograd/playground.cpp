9a6efc6ef4caedec0bf8d3eec24438a79b397682
10-zin <tenzinbhotia0@gmail.com>
2023-06-22 19:14:29 +0530

    playground for backward

--- cpp-micrograd/playground.cpp
+++ cpp-micrograd/playground.cpp
@@ -6,16 +6,28 @@ int main() {
     // ...
     std::shared_ptr<Value> value1 = std::make_shared<Value>(2.5);
     std::shared_ptr<Value> value2 = std::make_shared<Value>(3.7);
+    std::shared_ptr<Value> value3 = std::make_shared<Value>(-3.0);
+    std::shared_ptr<Value> value4 = std::make_shared<Value>(1.7);
 
     // Perform addition using the operator+
-    std::shared_ptr<Value> result_add = value1 + value2;
+    std::shared_ptr<Value> value1_2 = value1 + value2;
 
     // Perform multiplication using the operator*
-    std::shared_ptr<Value> result_mul = value1 * value2;
+    std::shared_ptr<Value> value_1_2_3 = value1_2 * value3;
+
+    std::shared_ptr<Value> result_final = value_1_2_3+value4;
+
 
     // Access the result and print the data
-    std::cout << "Result ADD: " << result_add->get_data() << std::endl;
-    std::cout << "Result MUL: " << result_mul->get_data() << std::endl;
+    std::cout << "Result Final: " << result_final->get_data() << std::endl;
+    // result_final->set_grad(1.0);
+    result_final->backward();
+    std::cout<<"\n\n\n";
+    std::cout<<value1->get_grad();
+    std::cout<<value2->get_grad();
+    std::cout<<value3->get_grad();
+    std::cout<<value4->get_grad();
+    // std::cout << "Result MUL: " << result_mul->get_data() << std::endl;
     
     return 0;
 }
