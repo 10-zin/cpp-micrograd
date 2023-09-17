25ef0175dd0eea4846bea1f5dca859ca7782acae
10-zin <tenzinbhotia0@gmail.com>
2023-09-16 21:23:18 -0400

    fix major loss re-init bug

--- c-micrograd/train.c
+++ c-micrograd/train.c
@@ -22,33 +22,27 @@ int main() {
     int labels=2;
     int sizes[] = {1, 5, 10, 5, labels};
     int nlayers = sizeof(sizes) / sizeof(int);
-    int backward_freq = 1;
+    int backward_freq = 2;
     printf("nlayers:%i\n", nlayers);
     MLP* mlp = init_mlp(sizes, nlayers);
 
     Entry* entries = load_data();
 
-
-    // for (int i =0; i < 200; i++) {
-    //     printf("%d\n", entries[i].label);
-    // }
-    // exit(1);
-
     // Train for a few iterations
-    int epochs = 105;
+    int epochs = 50;
     float lr = 0.001;
     Value* total_loss = make_value(0.0);
     float epoch_loss = 0.0;
     // show_params(mlp);
-    for (int i = 0; i < epochs; i++) {
+    for (int ep = 0; ep < epochs; ep++) {
         for (int i =0; i < 25; i++) {
+            
             float arr_x[] = {entries[i].number};
-            // printf("kk->%f\n", entries[i].label);
-            float* arr_y = one_hot_encode(entries[i].label);
-            // float arr_y[] = {entries[i].label};
             Value** x = make_values(arr_x);
+
+            float* arr_y = one_hot_encode(entries[i].label);
             Value** y_true = make_values(arr_y);
-            // printf("y_trueee%f %f", y_true[0]->val, y_true[1]->val);
+
             Value* loss = train(mlp, x, y_true, lr);
             total_loss = add(total_loss, loss);
             epoch_loss+=total_loss->val;
@@ -56,7 +50,7 @@ int main() {
             // Backward pass
             if (i%backward_freq==0){
                 total_loss->grad=1.0;
-                backward(loss);
+                backward(total_loss);
                 total_loss = make_value(0.0);
             }
             
@@ -64,7 +58,8 @@ int main() {
             // show_params(mlp);
         }
         printf("\n\nEPOCH LOSSS!!::%f: \n\n", epoch_loss/25);
-        float epoch_loss=0.0;
+        epoch_loss=0.0;
+        
         
     }
 
